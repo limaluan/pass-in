db8319562a3a84c18274373683a5d3b93812943a
Luan Lima <limaluan.dev@gmail.com>
2024-04-02 18:54:15 -0300

    feat: adds controllers

---
+++ src/main/java/limadev/passin/controllers/AttendeeController.java
@@ -0,0 +1,9 @@
+package limadev.passin.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/attendees")
+public class AttendeeController {
+}
