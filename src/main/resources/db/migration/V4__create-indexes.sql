2153877fb4127a02a0e949a9a88e90f44a13c2ea
Luan Lima <limaluan.dev@gmail.com>
2024-04-02 18:52:54 -0300

    feat: adds Entities
     - adds: Attendee Entity
     - adds: Event Entity
     - adds: CheckIn Entity

--- src/main/resources/db/migration/V4__create-indexes.sql
+++ src/main/resources/db/migration/V4__create-indexes.sql
@@ -1,3 +1,9 @@
+-- Isso garante que todos os valores na coluna 'slug' sejam exclusivos
 CREATE UNIQUE INDEX events_slug_key ON events(slug);
-CREATE UNIQUE INDEX attendees_event_id_email_key ON attendees (event_id, email);
-CREATE UNIQUE INDEX check_ins_attendee_id_key ON check_ins (attendee_id); 
+
+-- Isso garante que a combinação desses valores não se repitam
+-- Ou seja, ele vai garantir que não exista nenhum participante (email) em dois eventos (event_id) e vice-versa
+CREATE UNIQUE INDEX attendees_event_id_email_key ON attendees(event_id, email);
+
+-- E isso garante que o usuário (attendee_id) não faça mais de um Check in
+CREATE UNIQUE INDEX check_ins_attendee_id_key ON check_ins(attendee_id); 
